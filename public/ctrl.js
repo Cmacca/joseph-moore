247d5360b5efacbe8bd66d8fdc45ac1ce27cc505
Krish Dholakiya <krishna.dholakiya@gmail.com>
2013-08-25 18:02:10 -0600

    Added Shipping and Handling page with form sending data to model and localStorage API, along with validation

--- public/ctrl.js
+++ public/ctrl.js
@@ -2,6 +2,7 @@ function pageCtrl($scope) {
 	$scope.priceTotal = $scope.bookQuantity * 7.99;
 
 	$('#payit').hide();
+	$('.next-but').attr("style", "display:none !important");
 
 	$scope.changeQuantity = function() {
 		localStorage.bookQuantity = $scope.bookQuantity;
@@ -39,7 +40,38 @@ function pageCtrl($scope) {
 	}
 	};
 	
-	var toChange = function(zeModel) {
-		localStorage.zeModel = $scope.zeModel;
-	}
+	$scope.accepted = function() {
+ 		localStorage.acceptIt = $scope.sAccept;
+ 		console.log("cahnged")
+ 		if (localStorage.sName.length > 0 && localStorage.sAddress.length > 0 && localStorage.sCity.length > 0 && localStorage.sState.length > 0 && localStorage.sZip.length > 0 && localStorage.acceptIt == 'true') {
+ 			$('.next-but').show();
+ 		}
+ 		else if (localStorage.acceptIt == "false" || localStorage.sName.length <= 0 || localStorage.sAddress.length <= 0 || localStorage.sCity.length <= 0 || localStorage.sState.length <= 0 || localStorage.sZip.length <= 0) {
+ 			$('.next-but').attr("style", "display: none;");
+ 		}
+ 	}
+
+ 	$scope.snhName = function() {
+ 		localStorage.sName = $scope.sName;
+ 		$scope.accepted();
+ 	}
+ 	$scope.snhAddress = function() {
+ 		localStorage.sAddress = $scope.sAddress;
+ 		$scope.accepted();
+ 	}
+ 	$scope.snhCity = function() {
+ 		localStorage.sCity = $scope.sCity;
+ 		$scope.accepted();
+ 	}
+ 	$scope.snhState = function() {
+ 		localStorage.sState = $scope.sState;
+ 		$scope.accepted();
+ 				
+ 	}
+ 	$scope.snhZip = function() {
+ 		localStorage.sZip = $scope.sZip;
+ 		$scope.accepted();
+ 	}
+
+
 }
